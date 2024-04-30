2e5c7e2602284d8e294bbb75d5cb953e5485a1d2
Adam <adamziv156@gmail.com>
2024-04-30 08:06:55 -0500

    just opening my pull

--- src/main/java/library/Inventory.java
+++ src/main/java/library/Inventory.java
@@ -1,4 +1,27 @@
 package library;
 
+import java.util.ArrayList;
+
 public class Inventory {
+    public static ArrayList<Material> materials = new ArrayList<>();
+
+    /**
+     * I have to figure out what this method will be used for
+     * @param checkingOut
+     */
+    public void checkoutMaterial(String checkingOut){
+
+    }
+
+
+
+
+
+    public ArrayList<Material> getMaterials() {
+        return materials;
+    }
+
+    public void setMaterials(ArrayList<Material> materials) {
+        this.materials = materials;
+    }
 }
